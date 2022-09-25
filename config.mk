c3080f0827cdfe0f2e2f2c662275cc01d2d9a1dd
Kollins <kgl2@cin.ufpe.br>
2022-09-25 15:48:27 -0300

    CommunicationManager Makefile

---
+++ config.mk
@@ -0,0 +1,22 @@
+# version
+VERSION = 0.1
+
+# paths
+DEST 	:= /opt/fls
+DEPS 	:= ARINC615AManager
+
+INSTALL_PATH 	:= $(DEST)
+DEP_PATH 		:= $(DEST)
+
+CXX 		?=
+CXXFLAGS 	:= -Wall -Werror -std=c++11 -pthread
+DBGFLAGS 	:= -g -ggdb
+TESTFLAGS 	:= -fprofile-arcs -ftest-coverage --coverage -lgcov
+LINKFLAGS 	:= -shared
+LDFLAGS  	:= -L$(DEP_PATH)/lib
+LDLIBS   	:= -larinc615a
+
+COBJFLAGS 	:= $(CXXFLAGS) -c -fPIC
+test: COBJFLAGS 	+= $(TESTFLAGS)
+test: LINKFLAGS 	+= -lgcov
+debug: COBJFLAGS 	+= $(DBGFLAGS)
