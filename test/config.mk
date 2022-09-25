341f20bdd019955e9be4ce52eed49a210d74b610
Kollins <kgl2@cin.ufpe.br>
2022-09-25 15:49:32 -0300

    Basic unit test for CommunicationManager [PES-44]

---
+++ test/config.mk
@@ -0,0 +1,19 @@
+# version
+VERSION = 0.1
+
+GTEST_ROOT	:=./googletest
+DEP_PATH	:= /opt/fls
+
+CXX				?=
+CXXFLAGS 		+= -Wall
+CXXFLAGS 		+= -Wextra
+CXXFLAGS		+= -pthread
+CXXFLAGS 		+= -fprofile-arcs -ftest-coverage --coverage
+CXXFLAGS 		+= -g -ggdb
+COBJFLAGS 		:= $(CXXFLAGS) -c
+LDFLAGS  		:= -L$(GTEST_ROOT)/lib -L$(DEP_PATH)/lib
+LDLIBS   		:= -lcommunicationmanager -larinc615a -ltransfer -ltftp -ltftpd -lgtest -lgcov -lpthread -lcjson
+INCFLAGS 		:= -I$(GTEST_ROOT)/googletest/include -I$(DEP_PATH)/include
+
+DBGFLAGS 	:= -g -ggdb
+debug: COBJFLAGS 	+= $(DBGFLAGS)
---
+++ test/src/unity_test_basic.cpp
@@ -0,0 +1,106 @@
+#include <gtest/gtest.h>
+
+#include "communication_api.h"
+
+class CommunicationManagerTest : public ::testing::Test
+{
+protected:
+    CommunicationManagerTest()
+    {
+    }
+
+    ~CommunicationManagerTest() override
+    {
+    }
+
+    void SetUp() override
+    {
+        CommunicationOperationResult result = create_handler(&handler);
+        ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+        ASSERT_NE(handler, nullptr);
+    }
+
+    void TearDown() override
+    {
+        destroy_handler(&handler);
+        ASSERT_EQ(handler, nullptr);
+    }
+
+    CommunicationHandlerPtr handler;
+};
+
+TEST_F(CommunicationManagerTest, RegisterFindStartedCallback)
+{
+    find_started callback = [](CommunicationHandlerPtr handler,
+                               void *context)
+    {
+        return COMMUNICATION_OPERATION_OK;
+    };
+    void *context = nullptr;
+    CommunicationOperationResult result = register_find_started_callback(handler, callback, context);
+    ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+}
+
+TEST_F(CommunicationManagerTest, RegisterFindFinishedCallback)
+{
+    find_finished callback = [](CommunicationHandlerPtr handler,
+                                void *context)
+    {
+        return COMMUNICATION_OPERATION_OK;
+    };
+    void *context = nullptr;
+    CommunicationOperationResult result = register_find_finished_callback(handler, callback, context);
+    ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+}
+
+TEST_F(CommunicationManagerTest, RegisterFindNewDeviceCallback)
+{
+    find_new_device callback = [](CommunicationHandlerPtr handler,
+                                  const char *device,
+                                  void *context)
+    {
+        return COMMUNICATION_OPERATION_OK;
+    };
+    void *context = nullptr;
+    CommunicationOperationResult result = register_find_new_device_callback(handler, callback, context);
+    ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+}
+
+TEST_F(CommunicationManagerTest, RegisterUploadInitializationResponseCallback)
+{
+    upload_initialization_response_callback callback = [](CommunicationHandlerPtr handler,
+                                                          const char *response,
+                                                          void *context)
+    {
+        return COMMUNICATION_OPERATION_OK;
+    };
+    void *context = nullptr;
+    CommunicationOperationResult result = register_upload_initialization_response_callback(handler, callback, context);
+    ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+}
+
+TEST_F(CommunicationManagerTest, RegisterUploadInformationStatusCallback)
+{
+    upload_information_status_callback callback = [](CommunicationHandlerPtr handler,
+                                                     const char *status,
+                                                     void *context)
+    {
+        return COMMUNICATION_OPERATION_OK;
+    };
+    void *context = nullptr;
+    CommunicationOperationResult result = register_upload_information_status_callback(handler, callback, context);
+    ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+}
+
+TEST_F(CommunicationManagerTest, RegisterUploadFileNotAvailableCallback)
+{
+    file_not_available_callback callback = [](CommunicationHandlerPtr handler,
+                                              unsigned short *wait_time_s,
+                                              void *context)
+    {
+        return COMMUNICATION_OPERATION_OK;
+    };
+    void *context = nullptr;
+    CommunicationOperationResult result = register_file_not_available_callback(handler, callback, context);
+    ASSERT_EQ(result, COMMUNICATION_OPERATION_OK);
+}
